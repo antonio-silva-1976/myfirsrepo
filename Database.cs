788c8e454bf1f76cad0cf939530a1b3b2ec4b7cc
Antonio Marcos <toni.webmaster@gmail.com>
2023-03-15 08:22:57 -0300

    adicionando arquivo Database

---
+++ Database.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blog.Infra
+{
+    public static class Database
+    {
+        public static SqlConnection Connection;
+    }
+}
